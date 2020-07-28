ffb432f13d5ef76e3ac17d420f82a5e1f3f61172
ThanhNgo <thanhngo26@hotmail.com>
2020-07-28 10:19:21 -0700

    modified seeds queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- seeds queries fill the table with some data that we will be able to use and practice with
 USE burgers_db;
 INSERT INTO burgers(burger_name, devoured)
 VALUES ("hamburger",false), ("cheeseburger",false), ("veggieburger",false);
